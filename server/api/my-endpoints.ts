b6106bf0ae0a353de4846b0f0009baa3b38edf6c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-10-23 04:51:43 +0200

    chore(api): remove `async` specifier
    The function's body does not contain any `await` statement, so I propose to remove the `async` specifier.

--- server/api/my-endpoints.ts
+++ server/api/my-endpoints.ts
@@ -1,4 +1,4 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler((event) => {
   if (event.context.$sentry) {
     // Do something here
     event.context.$sentry.captureException(new Error('Test error'))
